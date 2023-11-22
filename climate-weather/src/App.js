ac0b9d353de4056985301d6e568016ee07277a62
Sophia S. Gonçalves <sophiasgs11@gmail.com>
2023-11-21 21:50:58 -0300

    criação do tabuleiro

--- climate-weather/src/App.js
+++ climate-weather/src/App.js
@@ -1,13 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Teste</h1>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-    </div>
-  );
+export default function App(){
+
+const tabu = {
+  display: 'flex',
+  flexDirection: 'colunm'
+}
+const tabulinha = {
+  display: 'flex',
+  flexDirection: 'row'
+}
+const casa = {
+  width: 100,
+  height:100,
+  display:'flex',
+  justifyContent: center,
+  alingItens: center,
+  flexDirection: 'row',
+  cursor: 'pointer',
+  fontSize: 60,
+  border: '1px solid #000',
 }
 
-export default App;
+const jogoInicial=[['','','' ], [ '','',''],['','','']]
+const [jogo,setJogo]=useState([['','',''],['','',''],['','','']])
+const [simboloAtual, setSimboloAtual]=useState('X')
+const [jogando, setJogando]=useState(true)
+
+const tabuleiro=(j)=>{
+
+  return(
+  
+  <div style={tabu}>
+  
+        <div style={tabulinha}>
+        
+              <div stye={casa} data-pos='00' onClick="">{j[0][0]}</div>
+              
+              <div stye={casa} data-pos='01' onClick="">{j[0][1]}</div>
+              
+              <div stye={casa} data-pos='02' onClick="">{j[0][2]}</div>
+        
+        </div>
+        
+        <div style={tabulinha}>
+        
+              <div stye={casa} data-pos='10' onClick="">{j[1][0]}</div>
+              
+              <div stye={casa} data-pos='11' onClick="">{j[1][1]}</div>
+              
+              <div stye={casa} data-pos='12' onClick="">{j[1][2]}</div>
+        
+        </div><div style={tabulinha}>
+        
+              <div stye={casa} data-pos='20' onClick="">{j[2][0]}</div>
+              
+              <div stye={casa} data-pos='21' onClick="">{j[2][1]}</div>
+              
+              <div stye={casa} data-pos='22' onClick="">{j[2][2]}</div>
+
+        </div>
+
+  </div>
+  
+  )
+  }
+
+return (
+  
+j
+
+    );
+};
